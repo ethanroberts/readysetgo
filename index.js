eb539d17ec1e0e8df06fe26827d0dc7dc3d48138
David Homewood <david.homewood@xecute.net>
2015-05-21 22:00:53 +1200

    corrected some brackets

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ app.get('/', function(request, response) {
 });
 
 app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM agency', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows); }
-    });
-  });
-}
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    	client.query('SELECT * FROM agency', function(err, result) {
+    		done();
+    		if (err)
+    			{ console.error(err); response.send("Error " + err); }
+    		else
+    			{ response.send(result.rows); }
+		});
+	});
+});
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));

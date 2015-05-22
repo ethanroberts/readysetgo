33ec4fbc1135469068f969e115604d998af3d61e
David Homewood <david.homewood@xecute.net>
2015-05-22 14:05:13 +1200

    changed greeting to Hello team before push to heroku

---
+++ index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var pg = require('pg');
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello Team G Elliot!');
+});
+
+app.get('/db', function (request, response) {
+	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    	client.query('SELECT * FROM agency', function(err, result) {
+    		done();
+    		if (err)
+    			{ console.error(err); response.send("Error " + err); }
+    		else
+    			{ response.send(result.rows); }
+		});
+	});
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});

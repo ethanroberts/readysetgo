896aa2cddf278d08798748c3df1bba9bcfdb7a5b
David Homewood <david.homewood@xecute.net>
2015-05-22 15:09:45 +1200

    added some more sql statements in as /db2 and /db3

--- index.js
+++ index.js
@@ -10,15 +10,39 @@ app.get('/', function(request, response) {
 });
 
 app.get('/db', function (request, response) {
-	pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    	client.query('SELECT * FROM agency', function(err, result) {
-    		done();
-    		if (err)
-    			{ console.error(err); response.send("Error " + err); }
-    		else
-    			{ response.send(result.rows); }
-		});
-	});
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT * FROM agency', function(err, result) {
+        done();
+        if (err)
+          { console.error(err); response.send("Error " + err); }
+        else
+          { response.send(result.rows); }
+    });
+  });
+});
+
+app.get('/db2', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT stop_id,stop_code,stop_name FROM stops WHERE stop_id = 11645', function(err, result) {
+        done();
+        if (err)
+          { console.error(err); response.send("Error " + err); }
+        else
+          { response.send(result.rows); }
+    });
+  });
+});
+
+app.get('/db3', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT trip_id,arrival_time,departure_time,stop_id,stop_sequence FROM stoptimes WHERE stop_id = 11645 LIMIT 15', function(err, result) {
+        done();
+        if (err)
+          { console.error(err); response.send("Error " + err); }
+        else
+          { response.send(result.rows); }
+    });
+  });
 });
 
 app.listen(app.get('port'), function() {

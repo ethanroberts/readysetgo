385319dbdcb206f515bfddb488802bda8a5e0142
David Homewood <david.homewood@xecute.net>
2015-06-01 20:16:09 +1200

    added a route path query returning lat/lon for the route of a trip

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ var pg = require('pg');
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-//app.get('/', function(request, response) {
-//  response.send('Hello Team G Elliot!');
-//});
 app.get('/', function(req, res) {
   res.sendfile('index.html', {root: __dirname })
 });
-
+// output stops
 app.get('/stops/:id', function (request, response) {
-  console.log('Call for id ', request.params.id);
+  console.log('Call on stops for id ', request.params.id);
   var sql = "SELECT stoptimes.trip_id, stop_sequence, arrival_time,stoptimes.stop_id, stop_code, stop_name, stop_desc, stop_lat, stop_lon FROM stoptimes INNER JOIN stops ON stops.stop_id = stoptimes.stop_id WHERE stoptimes.trip_id ="+request.params.id+";"
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
       client.query(sql,function(err, result) {
@@ -25,10 +22,12 @@ app.get('/stops/:id', function (request, response) {
     });
   });
 });
-
-app.get('/db', function (request, response) {
+// output route path
+app.get('/path/:id', function (request, response) {
+  console.log('Call on shapes (route path) for id ', request.params.id);
+  var sql = "SELECT trips.trip_id, shapes.shape_id, shape_pt_lat, shape_pt_lon, shape_pt_sequence FROM trips INNER JOIN shapes ON shapes.shape_id = trips.shape_id WHERE trips.trip_id = "+request.params.id+";"
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-      client.query('SELECT * FROM agency', function(err, result) {
+      client.query(sql,function(err, result) {
         done();
         if (err)
           { console.error(err); response.send("Error " + err); }
